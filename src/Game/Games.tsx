127268a8f56fb092bb21c3c6b497f6dd3f1496d6
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-03-30 14:10:56 +0100

    make game dynamic

--- src/Game/Games.tsx
+++ src/Game/Games.tsx
@@ -2,14 +2,93 @@ import './Games.css';
 import abi from '../JigsawABI.json'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import { ethers } from 'ethers';
+import { useState, useEffect } from 'react';
 
 const contractAddress = "0xB30d25a037AefD6C90B4F6f8a3333bbb832F9385"
+const infuraUrl = `https://rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`
+const provider = new ethers.providers.JsonRpcProvider(infuraUrl)
+const contract = new ethers.Contract(contractAddress, abi, provider);
+
+
 export default function Game() {
 
-    function validGames() {
+    const [valid, setValid] = useState<{src: string, name: string, description: string}[]>([]);
+    const [invalid, setInvalid] = useState<{src: string, name: string, description: string}[]>([]);
 
+    // const validCarousel = document.getElementsByTagName('Carousel')[0] as HTMLElement;
+    // const invalidCarousel = document.getElementsByTagName('Carousel')[1] as HTMLElement;
+
+    async function getNFT(metadataUrl: string) {
+        const metadata = await (await fetch(metadataUrl)).json();
+        const imageUrl = metadata["image"]
+        const name = metadata["name"]
+        const description = metadata["description"]
+
+        return {
+            src: imageUrl,
+            name: name,
+            description: description,
+        }
     }
-    
+
+    async function validGames() {
+        const uris = await contract.getUris(1, 6, false)
+        console.log(uris)
+        for (let i = 0; i < uris.length; i++) {
+            const metadataUrl = uris[i]
+            const nft = await getNFT(metadataUrl)
+            setValid(prevState => [...prevState!, nft])
+        }
+    }
+
+    async function invalidGames() {
+        const uris = await contract.getUris(1, 6, true)
+        for (let i = 0; i < uris.length; i++) {
+            const metadataUrl = uris[i]
+            const nft = await getNFT(metadataUrl)
+            setInvalid(prevState => [...prevState!, nft])
+        }
+    }
+
+    useEffect(() => {
+        validGames()
+        invalidGames()
+    })
+
+    useEffect(() => {
+        var newCarousel = ""
+        if (valid!.length > 0) {
+            for (let i = 0; i < valid!.length; i++) {
+                newCarousel += `
+                <div className='game'>
+                    <img src="${valid![i].src}" alt="" />
+                    <h3>${valid![i].name}</h3>
+                    <p>${valid![i].description}</p>
+                    <button>Test</button>
+                </div>
+                `
+            }
+        }
+        // validCarousel.innerHTML = newCarousel
+
+    }, [valid])
+
+    useEffect(() => {
+        var newCarousel = ""
+        for (let i = 0; i < invalid!.length; i++) {
+            newCarousel += `
+            <div className='game'>
+                <img src="${invalid![i].src}" alt="" />
+                <h3>${invalid![i].name}</h3>
+                <p>${invalid![i].description}</p>
+                <button>Test</button>
+            </div>
+            `
+        }
+        // invalidCarousel.innerHTML = newCarousel
+    }, [invalid])
+
     const responsive = {
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
