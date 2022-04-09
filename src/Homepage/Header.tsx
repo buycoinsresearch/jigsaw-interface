6dfa2ec03394695be660d7ec313e1215e7270141
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-04-09 18:48:58 +0100

    add chainId

--- src/Homepage/Header.tsx
+++ src/Homepage/Header.tsx
@@ -4,13 +4,17 @@ import { ethers, providers } from 'ethers';
 import detectEthereumProvider from '@metamask/detect-provider';
 import WalletConnect from "@walletconnect/client";
 import QRCodeModal from "@walletconnect/qrcode-modal";
-import WalletConnectProvider from "@walletconnect/web3-provider";
 global.Buffer = global.Buffer || require('buffer').Buffer;
 
 function Header() {
     const [address, setAddress] = useState('');
     const [home, setHome] = useState('');
-    const [injected, setInjected] = useState(true)
+    const [chainId, setChainId] = useState<number>(0);
+    // Create a connector
+    const connector = new WalletConnect({
+        bridge: "https://bridge.walletconnect.org", // Required
+        qrcodeModal: QRCodeModal,
+    });
 
     async function connect() {
         const provider = await detectEthereumProvider();
@@ -21,20 +25,52 @@ function Header() {
             setAddress(accounts[0]);
             window.localStorage.setItem('address', accounts[0]);
         } else {
-            // Create a connector
-            const connector = new WalletConnect({
-                bridge: "https://bridge.walletconnect.org", // Required
-                qrcodeModal: QRCodeModal,
-            });
+            
             
             // Check if connection is already established
             if (!connector.connected) {
                 // create new session
-                console.log(connector)
-                connector.createSession();
+                await connector.createSession();
+                const session = window.localStorage.getItem('walletconnect');
+                const parsedSession = JSON.parse(session!);
+                setAddress(parsedSession.accounts[0]);
+                setChainId(parsedSession.chainId);
+                
             }
+
         }
     }
+
+    // Subscribe to connection events
+    connector.on("connect", (error, payload) => {
+        if (error) {
+        throw error;
+        }
+    
+        // Get provided accounts and chainId
+        const { accounts, chainId } = payload.params[0];
+        setAddress(accounts[0]);
+        setChainId(chainId);
+    });
+    
+    connector.on("session_update", (error, payload) => {
+        if (error) {
+        throw error;
+        }
+    
+        // Get updated accounts and chainId
+        const { accounts, chainId } = payload.params[0];
+        setAddress(accounts[0]);
+        setChainId(chainId);
+    });
+    
+    connector.on("disconnect", (error, payload) => {
+        if (error) {
+        throw error;
+        }
+    
+        // Delete connector
+    });
 
     useEffect(() => {
         if (window.localStorage.getItem('address')) {
@@ -45,21 +81,23 @@ function Header() {
             const slicedAddress = address.slice(0, 6) + "..." + address.slice(-4);
             connect.innerHTML =  slicedAddress;
         }
-        
+        if (chainId !== 4 && chainId !== 0) {
+            alert('Please connect to Rinkeby testnet')
+            console.log(chainId);
+        }
         const home = window.location.href.split("/");
         setHome("http://" + home[2]);
     }, [address])
 
+    useEffect(() => {
+        if (chainId !== 4 && chainId !== 0) {
+            alert('Please connect to Rinkeby testnet')
+            console.log(chainId);
+        }
+    }, [chainId])
+
     return (
         <div className="App-header">
-            {/* {account 
-                ? <button onClick={() => deactivate()}>Disconnect</button> 
-                : <button onClick={() => {
-                    activate(WalletConnect, undefined, true)
-                    .then((resp) => console.log(resp))
-                    .catch((err) => console.log(err))
-                }}>Connect</button>
-            } */}
         <a href={home}><img src={logo} className="App-logo" alt="logo" /></a>
         <button id="connect-wallet" onClick={connect}>Connect wallet</button>
         </div>
