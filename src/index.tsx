3155fe7c363ce66e73f867dc765d982512e68546
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-04-10 13:45:08 +0100

    add metadata

--- src/index.tsx
+++ src/index.tsx
@@ -2,31 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import WalletConnectProvider from '@walletconnect/react-native-dapp';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import Jigsaw from './route';
-import { ethers } from 'ethers';
-
-const POLLING_INTERVAL = 12000;
-
-const getLibrary = (provider: any) => {
-  const library = new ethers.providers.Web3Provider(provider);
-  return library;
-};
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Web3ReactProvider getLibrary={getLibrary}> */}
-    {/* <WalletConnectProvider
-      redirectUrl={window.location.href}
-      storageOptions= {{
-        // asyncStorage: AsyncStorage,
-      }}> */}
       <Jigsaw />
-      {/* </WalletConnectProvider> */}
-    {/* </Web3ReactProvider> */}
-      
-    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
